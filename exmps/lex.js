8693c64f5211d90d03c8525723f89436ecf4c7ba
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-03-03 22:14:48 -0500

    Lexer works

--- exmps/lex.js
+++ exmps/lex.js
@@ -1,3 +1,10 @@
 import Lexer from '@cato-script/lexer';
 
-const lexer = new Lexer('');
+const lexer = new Lexer(`5 + 5
++ 10`);
+
+for (let i = 0; i < lexer.count; i++) {
+  const token = lexer.peek(i);
+  console.log(token);
+}
+
--- packages/lexer/src/index.ts
+++ packages/lexer/src/index.ts
@@ -1,10 +1,58 @@
-/**
- * The basic lexer for CSI which splits all characters into position tokens.
- */
+export interface LexerChar {
+	char: string;
+	index: number;
+	line: number;
+	column: number;
+}
+
 export default class Lexer {
-	public source: string;
+	#source: string;
+	#positionals: LexerChar[];
+	#index = 0;
 
 	public constructor(source: string) {
-		this.source = source;
+		this.#source = source;
+		this.#positionals = [];
+
+		let currentIndex = 0;
+
+		source.split('\n').forEach((lineString, lineNumber) => {
+			lineString.split('').forEach((charString, charIndex) => {
+				this.#positionals.push({
+					char: charString,
+					index: currentIndex,
+					line: lineNumber + 1,
+					column: charIndex
+				});
+
+				currentIndex++;
+			});
+		});
+
+		console.log(this.#positionals)
+	}
+
+	public peek(offset: number = 1) {
+		return this.#positionals[this.#index + offset];
+	}
+
+	public get next() {
+		return this.#positionals[this.#index + 1];
+	}
+
+	public get prev() {
+		return this.#positionals[this.#index + 1];
+	}
+
+	public jumpNext(offset: number = 1) {
+		this.#index += offset;
+	}
+
+	public jumpPrev(offset: number = 1) {
+		this.#index -= offset;
+	}
+
+	public get count() {
+		return this.#positionals.length;
 	}
 }
