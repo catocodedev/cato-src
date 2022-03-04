fd766b0264d5d9fa13bd50e1ce32952fe28868bc
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-03-03 23:45:39 -0500

    Updated example

--- exmps/token.js
+++ exmps/token.js
@@ -2,7 +2,9 @@ import Tokenizer from "@cato-script/tokenizer";
 import Lexer from "@cato-script/lexer";
 
 const lexer = new Lexer([
-    'if (5 == 5)'
+    'if (5 == 5) {',
+    '    meow "If statement is true";' +
+    '}'
 ].join('\n'));
 
 const tokenizer = new Tokenizer(lexer);
